1e26f86bfd236bf7c06e9fb72c5e2e8affa290b9
T35000139 <icaro.baruffi@compasso.com.br>
2021-01-29 02:03:57 -0300

    pureJS-001 criação de componentes js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import "./styles.less";
 import api from "./api/api.js";
+import card from "./components/card/card";
 
-console.log("hello world!");
 api.getMyBooks().then((result) => {
-  console.log(result);
+  result.books.forEach(function (book) {
+    var cardDOM = card.getCard.bind(book).call();
+    switch (book.shelf) {
+      case "currentlyReading":
+        document.querySelector("#books-currently-reading ol").appendChild(document.createElement("li")).appendChild(cardDOM);
+        break;
+      case "wantToRead":
+        document.querySelector("#books-want-to-read ol").appendChild(document.createElement("li")).appendChild(cardDOM);
+        break;
+      case "read":
+        document.querySelector("#books-read ol").appendChild(document.createElement("li")).appendChild(cardDOM);
+        break;
+    }
+
+  });
+
 });
+
+// Por enquanto até eu entender essa bagaça
+window.allowDrop = (ev) => {
+  ev.preventDefault();
+};
+
+window.drag = (ev) => {
+  ev.dataTransfer.setData("text", ev.target.id);
+};
+
+window.drop = (ev) => {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.path.forEach((ob) => {
+    if (ob.nodeName == "OL") {
+      ob.appendChild(document.getElementById(data));
+    }
+  });
+};
